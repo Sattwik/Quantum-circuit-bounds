57e0c4dff94df6b74d4367002eb880cbb0152e7a
Sattwik Deb Mishra <sdmishra@stanford.edu>
2021-11-27 19:20:36 +0530

    Test3 in test_dual.py works. Needed to reorder output edges after tn.contract_between() to make this work. Need to test multi step circuit, noise layer, check why the application of mixing layer apparently doesn't change the density matrix, and convert everything to a jax backend to implement the analytical gradient.

--- test_dual.py
+++ test_dual.py
@@ -41,13 +41,41 @@ p = 1
 # direct computation
 # FAILED: error in problem unitary in dual
 
-gamma = np.array([np.pi/4])
-beta = np.array([0])
+# gamma = np.array([np.pi/4])
+# beta = np.array([np.pi/19])
+#
+# dual_obj = dual.MaxCutDual(prob_obj = maxcut_obj, p = p, gamma = gamma, beta = beta, p_noise = 0)
+# state = dual_obj.circuit_layer(layer_num = 0, var_tensor = dual_obj.rho_init_tensor)
+#
+# state_dm = dual_obj.tensor_2_mat(state.tensor)
+#
+# psi_after_step = maxcut_obj.init_state()
+# psi_after_step = maxcut_obj.Up(gamma[0]) * psi_after_step
+# psi_after_step = maxcut_obj.Um(beta[0]) * psi_after_step
+#
+# rho_after_step = psi_after_step * psi_after_step.dag()
+#
+# rho = rho_after_step.full()
+#
+# print(np.linalg.norm(state_dm - rho))
+
+# TEST3: gamma != 0, see if the superoperator unitary works with just two
+# sites
+
+gamma0 = np.pi/2
+beta0 = np.pi/19
+
+gamma = np.array([gamma0])
+beta = np.array([beta0])
+
+lattice = graphs.define_lattice(m = 1, n = 2)
+maxcut_obj = problems.MaxCut(lattice, lattice)
+p = 1
 
 dual_obj = dual.MaxCutDual(prob_obj = maxcut_obj, p = p, gamma = gamma, beta = beta, p_noise = 0)
 state = dual_obj.circuit_layer(layer_num = 0, var_tensor = dual_obj.rho_init_tensor)
 
-state_dm = np.reshape(state.tensor, (dual_obj.dim, dual_obj.dim))
+state_dm = dual_obj.tensor_2_mat(state.tensor)
 
 psi_after_step = maxcut_obj.init_state()
 psi_after_step = maxcut_obj.Up(gamma[0]) * psi_after_step
@@ -55,32 +83,32 @@ psi_after_step = maxcut_obj.Um(beta[0]) * psi_after_step
 
 rho_after_step = psi_after_step * psi_after_step.dag()
 
+rho_init_direct = (maxcut_obj.init_state() * maxcut_obj.init_state().dag()).full()
+
 rho = rho_after_step.full()
 
+Hp = -0.5*(qutip.tensor(qutip.qeye(2), qutip.qeye(2)) - qutip.tensor(qutip.sigmaz(), qutip.sigmaz()))
+U = (-1j * Hp * gamma0).expm().full()
+U_dag = U.conj().T
+
+# (ja jb) (ia ib) -> (ja jb ia ib)
+U_tensor = U.flatten()
+# (ja jb ia ib) -> (ja) (jb) (ia) (ib)
+U_tensor = U_tensor.reshape((2,2,2,2))
+# (ja) (jb) (ia) (ib) -> (ja) (ia) (jb) (ib)
+U_tensor = U_tensor.transpose([0, 2, 1, 3])
+
+# (iap ibp) (kap kbp) -> (iap ibp kap kbp)
+U_dag_tensor = U_dag.flatten()
+# (iap ibp kap kbp) -> (iap) (ibp) (kap) (kbp)
+U_dag_tensor = U_dag_tensor.reshape((2,2,2,2))
+# (iap) (ibp) (kap) (kbp) -> (iap) (kap) (ibp) (kbp)
+U_dag_tensor = U_dag_tensor.transpose([0, 2, 1, 3])
+
+# rho_tdot = dual_obj.rho_init_tensor.tensor
+# rho_tdot = np.tensordot(U_tensor, rho_tdot, axes = ([1, 3], [0, 2]))
+# rho_tdot = rho_tdot.transpose([])
+# rho_tdot = np.tensordot(U_dag_tensor, rho_tdot, axes = ([0, 2], [1, 3]))
+# rho_tdot = dual_obj.tensor_2_mat(rho_tdot)
+
 print(np.linalg.norm(state_dm - rho))
-
-# TEST3: gamma != 0, see if the superoperator unitary works with just two
-# sites
-
-gamma = np.pi/4
-
-Hp = -0.5 * (qutip.tensor(qutip.qeye(2), qutip.qeye(2)) - qutip.tensor(qutip.sigmaz(), qutip.sigmaz()))
-
-U = np.diag([1, np.exp(1j * gamma), np.exp(1j * gamma), 1])
-
-U_direct = (-1j * gamma * Hp).expm()
-
-state_init = qutip.rand_ket(4, dims = [[2, 2], [1, 1]])
-rho_init = (state_init * state_init.dag()).full()
-rho_init_tensor = tn.Node(rho_init)
-U_node = tn.Node(dual_obj.reshapeU_2site(U))
-
-new_edge_j = U_node[2] ^ rho_init_tensor[0]
-new_edge_k = U_node[3] ^ rho_init_tensor[1]
-
-rho_tensor = U_node @ rho_init_tensor
-
-state_direct = U_direct * state_init
-rho_direct = (state_direct * state_direct.dag()).full()
-
-print(np.linalg.norm(rho_direct - rho_tensor.tensor))
--- vqa/dual.py
+++ vqa/dual.py
@@ -17,7 +17,7 @@ class MaxCutDual():
 
     Members:
 
-        self.Sigmas: list of tensors each with N legs of dimension d^2 (d = 2
+        self.Sigmas: list of tensors each with 2N legs of dimension d (d = 2
                 by default). If each sigma is self-adjoint, the dual objective
                 function is real.
 
@@ -58,7 +58,7 @@ class MaxCutDual():
         self.p_noise = p_noise
         self.psi_init = prob_obj.init_state()
 
-        self.dim_list = tuple([self.local_dim ** 2] * self.num_sites_in_lattice)
+        self.dim_list = tuple([self.local_dim] * 2 * self.num_sites_in_lattice)
         self.dim = self.local_dim ** self.num_sites_in_lattice
 
         self.utri_indices = np.triu_indices(self.dim, 1)
@@ -67,8 +67,8 @@ class MaxCutDual():
         self.num_diag_elements = self.dim
 
         # constructing initial density operator
-        self.rho_init = self.psi_init * self.psi_init.dag()
-        self.rho_init_tensor = tn.Node(np.reshape(self.rho_init.full(), self.dim_list))
+        self.rho_init = (self.psi_init * self.psi_init.dag()).full()
+        self.rho_init_tensor = tn.Node(self.mat_2_tensor(self.rho_init), name = "rho_init")
 
         # initialising the noise channel superoperator
         # NB - the basis is |0X0|,|0X1|, |1X0|, |1X1|
@@ -82,6 +82,56 @@ class MaxCutDual():
 
         # self.tot_num_vars_per_step = 1 + self.num_diag_elements + 2 * self.num_tri_elements
         # lambda + number of elements in sigma
+
+    def mat_2_tensor(self, A: np.array):
+
+        """
+        Converts an array A that is indexed as (i1 i2 ... iN)(i1p i2p ... iNp)
+        into a tensor indexed as (i1) (i1p) (i2) (i2p) ... (iN) (iNp). () denote
+        composite indices.
+        """
+
+        # (i1 i2 ... iN)(i1p i2p ... iNp) -> (i1 i2 ... iN i1p i2p ... iNp)
+        T = A.flatten()
+
+        # (i1 i2 ... iN i1p i2p ... iNp) -> (i1) (i2) ... (iN) (i1p) (i2p) ... (iNp)
+        T = T.reshape(tuple([self.local_dim] * self.num_sites_in_lattice * 2))
+
+        # (i1) (i2) ... (iN) (i1p) (i2p) ... (iNp) -> (i1) (i1p) ... (iN) (iNp)
+        # [0, N, 1, N + 1, ..., N - 1, 2N - 1]
+        i1 = np.arange(self.num_sites_in_lattice)
+        i2 = self.num_sites_in_lattice + i1
+        i  = np.zeros(2 * self.num_sites_in_lattice, dtype = int)
+        i[::2] = i1
+        i[1::2] = i2
+
+        T = T.transpose(tuple(i))
+
+        return T
+
+    def tensor_2_mat(self, T: np.array):
+
+        """
+        Converts a tensor T that is indexed as  (i1) (i1p) (i2) (i2p) ... (iN) (iNp)
+        into a matrix indexed as (i1 i2 ... iN)(i1p i2p ... iNp). () denote
+        composite indices.
+        """
+
+        # (i1) (i1p) (i2) (i2p) ... (iN) (iNp) -> (i1) (i2) ... (iN) (i1p) (i2p) ... (iNp)
+        # [0, 2, 4, ..., 2N - 2, 1, 3, ..., 2N - 1]
+        i1 = np.arange(0, 2 * self.num_sites_in_lattice - 1, 2)
+        i2 = np.arange(1, 2 * self.num_sites_in_lattice, 2)
+        i = np.concatenate((i1, i2))
+
+        A = T.transpose(tuple(i))
+
+        # (i1) (i2) ... (iN) (i1p) (i2p) ... (iNp) -> (i1 i2 ... iN i1p i2p ... iNp)
+        A = A.flatten()
+
+        # (i1 i2 ... iN i1p i2p ... iNp) -> (i1 i2 ... iN)(i1p i2p ... iNp)
+        A = A.reshape((self.dim, self.dim))
+
+        return A
 
     def init_entropy_bounds(self):
 
@@ -118,32 +168,31 @@ class MaxCutDual():
 
             vars_full = np.diag(vars_diag_list[i]) + tri_real + tri_imag
 
-            self.Sigmas.append(tn.Node(np.reshape(vars_full, self.dim_list)))
+            self.Sigmas.append(tn.Node(self.mat_2_tensor(vars_full)))
 
     def unvectorize_vars(self, vars_vec):
 
-        vars_vec_split = np.split(vars_vec, self.p)
+        self.Lambdas = vars_vec[:self.p]
+
+        vars_vec_split = np.split(vars_vec[self.p:], self.p)
         # split the variables into p equal arrays
 
         vars_diag_list = []
         vars_real_list = []
         vars_imag_list = []
-        self.Lambdas = []
 
         for i in range(self.p):
 
             # for each circuit step the variables are arranged as:
-            # [vars_diag, vars_real, vars_imag, lambda]
+            # [vars_diag, vars_real, vars_imag]
 
             vars_diag = vars_vec_split[i][:self.num_diag_elements]
             vars_real = vars_vec_split[i][self.num_diag_elements:self.num_diag_elements + self.num_tri_elements]
             vars_imag = vars_vec_split[i][self.num_diag_elements + self.num_tri_elements:-1]
-            lmbda     = vars_vec_split[i][-1]
 
             vars_diag_list.append(vars_diag)
             vars_real_list.append(vars_real)
             vars_imag_list.append(vars_imag)
-            self.Lambdas.append(lmbda)
 
         return vars_diag_list, vars_real_list, vars_imag_list
 
@@ -165,9 +214,9 @@ class MaxCutDual():
             cost += -self.Lambdas[i] * np.log(np.sum(np.exp(-Ei/self.Lambdas[i])), base = 2)
 
         # the initial state term
-        # TODO: this is probably wrong. Redo in terms of contractions
-        sigma1 = np.reshape(self.Sigmas[0].tensor, newshape = (self.dim, self.dim))
-        epsilon_1_rho = np.reshape(self.noisy_circuit_layer(i = 0).tensor, newshape = (self.dim, self.dim))
+        # TODO: will this be faster if expressed in terms of contractions?
+        sigma1 = self.tensor_2_mat(self.Sigmas[0].tensor)
+        epsilon_1_rho = self.tensor_2_mat(self.noisy_circuit_layer(i = 0).tensor)
         cost += -np.trace(sigma1 @ epsilon_1_rho)
 
         return cost
@@ -187,14 +236,14 @@ class MaxCutDual():
         if i == self.p - 1:
             # TODO: check that the tensor product/site num ordering is
             # consistent
-            Hi = np.reshape(self.Sigmas[i].tensor, newshape = (self.dim, self.dim)) + self.prob_obj.H
+            Hi = self.tensor_2_mat(self.Sigmas[i].tensor) + self.prob_obj.H.full()
 
             return Hi
 
         else:
             Hi = self.Sigmas[i].tensor - self.noisy_circuit_layer(i + 1).tensor
 
-            return np.reshape(Hi, newshape = (self.dim, self.dim))
+            return self.tensor_2_mat(Hi)
 
     def noise_layer(self, var_tensor: tn.Node):
 
@@ -202,97 +251,128 @@ class MaxCutDual():
         Applies depolarizing noise on the var_tensor at all sites.
         """
 
-        res_tensor = var_tensor
+        # (i1) (i1p) (i2) (i2p) ... (iN) (iNp) -> (i1 i1p) (i2 i2p) ... (iN iNp)
+        res = np.reshape(var_tensor.tensor, tuple([self.local_dim ** 2] * self.num_sites_in_lattice))
+        res_tensor = tn.Node(res)
 
         for site_num in range(self.num_sites_in_lattice):
 
             noise_node = tn.Node(self.noise_superop,
-                                 axis_names = ["up", "down"]) # shape = 4x4
+                                 axis_names = ["up", "down"], name = "noise_node") # shape = 4x4
 
             # TODO: check
             edge_connection = noise_node["down"] ^ res_tensor[site_num]
 
             # perform the contraction
-            res_tensor = noise_node @ res_tensor
+            new_edge_order = res_tensor.edges[:site_num] + [noise_node["up"]] +\
+                             res_tensor.edges[site_num + 1:]
+            res_tensor = tn.contract_between(noise_node, res_tensor, output_edge_order = new_edge_order)
+
+        final_res = np.reshape(res_tensor.tensor, self.dim_list)
+        res_tensor = tn.Node(final_res)
 
         return res_tensor
-
-    def reshapeU_2site(self, U: np.array):
-
-        """
-        Reshapes U \otimes U^* into the appropriate 4x4x4x4 tensor to apply to
-        a vectorized density matrix/dual variable.
-
-        Dev notes: doing this manually because I couldn't find a library that
-        can handle this.
-        """
-
-        U_superop = np.kron(U, np.conj(U))
-
-        U_superop_tensored = np.zeros((4,4,4,4), dtype = complex)
-
-        for alpha in range(16):
-
-            for beta in range(16):
-
-                j1, j3, k1p, k3p = [int(a) for a in list("{0:04b}".format(alpha))]
-                i1, i3, i1p, i3p = [int(b) for b in list("{0:04b}".format(beta))]
-
-                index_0 = int((str(j1) + str(k1p)), 2)
-                index_1 = int((str(j3) + str(k3p)), 2)
-                index_2 = int((str(i1) + str(i1p)), 2)
-                index_3 = int((str(i3) + str(i3p)), 2)
-
-                U_superop_tensored[index_0, index_1, index_2, index_3] = U_superop[alpha, beta]
-
-        return U_superop_tensored
 
     def circuit_layer(self, layer_num: int, var_tensor: tn.Node):
 
         res_tensor = var_tensor
 
-        # applying the problem unitary
+        #----- Applying the problem unitary -----#
         gamma = self.gamma[layer_num]
 
         # U = exp(-i gamma/2 w * (I - Z_j Z_k))
         U = np.diag([1, np.exp(1j * gamma), np.exp(1j * gamma), 1])
+        U_dag = U.conj().T
 
-        U_superop = self.reshapeU_2site(U)
+        # (ja jb) (ia ib) -> (ja jb ia ib)
+        U_tensor = U.flatten()
+        # (ja jb ia ib) -> (ja) (jb) (ia) (ib)
+        U_tensor = U_tensor.reshape((2,2,2,2))
+        # (ja) (jb) (ia) (ib) -> (ja) (ia) (jb) (ib)
+        U_tensor = U_tensor.transpose([0, 2, 1, 3])
+
+        # (iap ibp) (kap kbp) -> (iap ibp kap kbp)
+        U_dag_tensor = U_dag.flatten()
+        # (iap ibp kap kbp) -> (iap) (ibp) (kap) (kbp)
+        U_dag_tensor = U_dag_tensor.reshape((2,2,2,2))
+        # (iap) (ibp) (kap) (kbp) -> (iap) (kap) (ibp) (kbp)
+        U_dag_tensor = U_dag_tensor.transpose([0, 2, 1, 3])
 
         for edge in self.prob_obj.graph.edges:
 
-            site_num_j = self.prob_obj.site_nums[edge[0]]
-            site_num_k = self.prob_obj.site_nums[edge[1]]
+            site_num_a = min(self.prob_obj.site_nums[edge[0]], self.prob_obj.site_nums[edge[1]])
+            site_num_b = max(self.prob_obj.site_nums[edge[0]], self.prob_obj.site_nums[edge[1]])
+
+            ia = 2 * site_num_a
+            ib = 2 * site_num_b
+
+            iap = 2 * site_num_a + 1
+            ibp = 2 * site_num_b + 1
+
+            print("On edge: ", edge)
+            print("ia: ", ia)
+            print("ib: ", ib)
+            print("iap: ", iap)
+            print("ibp: ", ibp)
 
             # TODO: does this work? check with a trial unitary/state.
-            U_node = tn.Node(U_superop)
+            U_node      = tn.Node(U_tensor, axis_names = ["ja", "ia", "jb", "ib"], name = "U_node")
+            U_dag_node  = tn.Node(U_dag_tensor, axis_names = ["iap", "kap", "ibp", "kbp"], name = "U_dag_node")
+
+            # print(self.tensor_2_mat(U_dag_node.tensor))
+            # print(U_dag_node.tensor)
 
             # TODO: does this work? name edges for clarity?
             # assumption that tn.Node() orders the axes in the same order as
             # the input np.array
-            new_edge_j = U_node[2] ^ res_tensor[site_num_j]
-            new_edge_k = U_node[3] ^ res_tensor[site_num_k]
 
-            res_tensor = U_node @ res_tensor
+            edge_a = U_node["ia"] ^ res_tensor[ia]
+            edge_b = U_node["ib"] ^ res_tensor[ib]
 
-        # applying the mixing unitary
+            new_edge_order = res_tensor.edges[:ia] + [U_node["ja"]] +\
+                             res_tensor.edges[ia + 1: ib] + [U_node["jb"]] +\
+                             res_tensor.edges[ib + 1:]
+
+            res_tensor = tn.contract_between(U_node, res_tensor, output_edge_order = new_edge_order)
+
+            edge_a_p = U_dag_node["iap"] ^ res_tensor[iap]
+            edge_b_p = U_dag_node["ibp"] ^ res_tensor[ibp]
+
+            new_edge_order = res_tensor.edges[:iap] + [U_dag_node["kap"]] +\
+                             res_tensor.edges[iap + 1: ibp] + [U_dag_node["kbp"]] +\
+                             res_tensor.edges[ibp + 1:]
+
+            res_tensor = tn.contract_between(U_dag_node, res_tensor, output_edge_order = new_edge_order)
+
+        #----- Applying the mixing unitary -----#
         beta = self.beta[layer_num]
 
         sx = qutip.sigmax()
         Ux = (-1j * beta * sx).expm().full()
-
-        Ux_superop = np.kron(Ux, np.conj(Ux))
+        Ux_dag = Ux.conj().T
 
         for site_num in range(self.num_sites_in_lattice):
 
-            Ux_node = tn.Node(Ux_superop) # shape = 4x4
+            Ux_node = tn.Node(Ux, axis_names = ["ja","ia"])
+            Ux_dag_node = tn.Node(Ux_dag, axis_names = ["iap","kap"])
+
+            ia  = 2 * site_num
+            iap = 2 * site_num + 1
 
             # TODO: check and name edges to improve readability
-            edge_connection = Ux_node[1] ^ res_tensor[site_num]
+            edge_a      = Ux_node["ia"] ^ res_tensor[ia]
+            edge_a_p    = Ux_dag_node["iap"] ^ res_tensor[iap]
 
             # TODO: check if re-assignment works
             # perform the contraction
-            res_tensor = Ux_node @ res_tensor
+
+            new_edge_order = res_tensor.edges[:ia] + [Ux_node["ja"]] +\
+                             res_tensor.edges[ia + 1:]
+            res_tensor = tn.contract_between(Ux_node, res_tensor, output_edge_order = new_edge_order)
+
+            new_edge_order = res_tensor.edges[:iap] + [Ux_dag_node["kap"]] +\
+                             res_tensor.edges[iap + 1:]
+            res_tensor = tn.contract_between(Ux_dag_node, res_tensor, output_edge_order = new_edge_order)
 
         return res_tensor
 
