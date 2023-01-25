f11fcfdf99809f2ab6186d77c6a1f8df4f445b94
Sattwik Deb Mishra <sdmishra@stanford.edu>
2023-01-25 02:00:33 -0800

    jax qaoa done.

--- vqa_bounds/qaoa.py
+++ vqa_bounds/qaoa.py
@@ -12,8 +12,8 @@ config.update("jax_enable_x64", True)
 import tensornetwork as tn
 tn.set_default_backend("jax")
 import scipy.optimize
+# import qutip
 
-# from vqa_bounds import graphs
 import graphs
 
 def maxcut_ham(graph):
@@ -37,24 +37,24 @@ def maxcut_ham(graph):
         Zk = Z_ops[edge[1]]
         H += -1/2 * (identity_all - Zj * Zk)
 
-    return H
+    return jnp.diag(H)
 
-@partial(jit, static_argnums = (1,2))
+# Note: Jitting (just-in-time compilation) here leads to long compile times
+# (much longer than the drop in execution time due to compilation)
+# due to for loops in the body. For loops can be worked around by
+# using jax.lax.fori_loop but this is good enough for now.
+# @partial(jit, static_argnums = (1,2))
 def qaoa_state(params, depth, graph):
     """
     Returns the state obtained by acting with the QAOA circuit on
     |+>^{\otimes len(graph.nodes)}.
     """
-    H = maxcut_ham(graph)
+    num_nodes = len(graph.nodes)
     xplus = 1/jnp.sqrt(2) * jnp.array([1, 1], dtype = complex)
     X = jnp.array([[0,1],[1,0]], dtype = complex)
-    num_nodes = len(graph.nodes)
+    I = jnp.identity(2)
 
-    Hmixing = 0
-    for i, node in enumerate(graph.nodes):
-        identity_left = jnp.identity(2 ** i)
-        identity_right = jnp.identity(2 ** (num_nodes - i - 1))
-        Hmixing += jnp.kron(identity_left, jnp.kron(X, identity_right))
+    H = maxcut_ham(graph)
 
     state = xplus
     for i in range(num_nodes - 1):
@@ -64,10 +64,12 @@ def qaoa_state(params, depth, graph):
     beta = params.at[depth:].get()
 
     for i in range(depth):
-        U = jax.scipy.linalg.expm(-1j * gamma.at[i].get() * H)
-        state = jnp.matmul(U, state)
+        # U = jax.scipy.linalg.expm(-1j * gamma.at[i].get() * H)
+        U = jnp.exp(-1j * gamma.at[i].get() * H)
+        state = U * state
 
-        sq_mixing = jax.scipy.linalg.expm(-1j * beta.at[i].get()/2 * X)
+        # sq_mixing = jax.scipy.linalg.expm(-1j * beta.at[i].get()/2 * X)
+        sq_mixing = jnp.cos(beta.at[i].get()/2) * I - 1j * jnp.sin(beta.at[i].get()/2) * X
         Umixing = 1
         for i in range(num_nodes):
             Umixing = jnp.kron(Umixing, sq_mixing)
@@ -75,7 +77,7 @@ def qaoa_state(params, depth, graph):
 
     return state
 
-@partial(jit, static_argnums = (1,2,3))
+# @partial(jit, static_argnums = (1,2,3))
 def expect_on_edge(params, depth, graph, edge):
     """
     Calculates the expectation value of the MAX-CUT Ham. term corresponding to
@@ -93,12 +95,13 @@ def expect_on_edge(params, depth, graph, edge):
         identity_right = jnp.identity(2 ** (num_nodes - i - 1))
         Z_ops[node] = jnp.kron(identity_left, jnp.kron(Z, identity_right))
     edge_ham = -1/2 * (identity_all - Z_ops[edge[0]] * Z_ops[edge[1]])
+    edge_ham = jnp.diag(edge_ham)
 
     state = qaoa_state(params, depth, graph)
 
-    return jnp.dot(state.conj(), jnp.matmul(edge_ham, state))
+    return jnp.dot(state.conj(), edge_ham * state)
 
-@partial(jit, static_argnums = (1,2))
+# @partial(jit, static_argnums = (1,2))
 def expect_on_full_ham(params, depth, graph):
     """
     Calculates the expectation value of the full MAX-CUT Ham. (all edges)
@@ -107,49 +110,50 @@ def expect_on_full_ham(params, depth, graph):
     state = qaoa_state(params, depth, graph)
     H = maxcut_ham(graph)
 
-    return jnp.dot(state.conj(), jnp.matmul(H, state))
+    return jnp.dot(state.conj(), H * state)
 
-@partial(jit, static_argnums = (1,2))
+# @partial(jit, static_argnums = (1,2))
 def obj_qaoa_maxcut_full(params, depth, graph):
     return jnp.real(expect_on_full_ham(params, depth, graph))
 
-@partial(jit, static_argnums = (1,2))
-def obj_qaoa_maxcut_cone(params, depth, graph):
-    edges, subgraphs = graphs.all_dist_p_subgraphs(graph, p = depth)
+# @partial(jit, static_argnums = (1,2))
+def obj_qaoa_maxcut_cone(params, depth, graph, edges, subgraphs):
+    # edges, subgraphs = graphs.all_dist_p_subgraphs(graph, p = depth)
     obj = 0
     for edge, subgraph in zip(edges, subgraphs):
         obj += expect_on_edge(params, depth, subgraph, edge)
     return jnp.real(obj)
 
-@partial(jit, static_argnums = (1,2))
+# @partial(jit, static_argnums = (1,2))
 def grad_qaoa_maxcut_full(params, depth, graph):
     return grad(obj_qaoa_maxcut_full, argnums = 0)(params, depth, graph)
 
-@partial(jit, static_argnums = (1,2))
-def grad_qaoa_maxcut_cone(params, depth, graph):
-    return grad(obj_qaoa_maxcut_cone, argnums = 0)(params, depth, graph)
+# @partial(jit, static_argnums = (1,2))
+def grad_qaoa_maxcut_cone(params, depth, graph, edges, subgraphs):
+    return grad(obj_qaoa_maxcut_cone, argnums = 0)(params, depth, graph, edges, subgraphs)
 
 def unjaxify_obj(func):
     def wrap(*args):
-        return float(func(jnp.array(args[0]), args[1], args[2]))
+        return float(func(jnp.array(args[0]), *args[1:]))
     return wrap
 
 def unjaxify_grad(func):
     def wrap(*args):
-        return np.array(func(jnp.array(args[0]), args[1], args[2]), order = 'F')
+        return np.array(func(jnp.array(args[0]), *args[1:]), order = 'F')
     return wrap
 
 def optimize_circ_params(mode,
-                params_init: np.array, depth, graph,
-                num_iters: int, opt_method: str = "L-BFGS-B"):
-    opt_args = (depth, graph)
+                params_init: np.array, depth, graph, edges = None, subgraphs = None,
+                num_iters: int = 50, opt_method: str = "L-BFGS-B"):
 
     if mode == "cone":
         obj_fun = obj_qaoa_maxcut_cone
         grad_fun = grad_qaoa_maxcut_cone
+        opt_args = (depth, graph, edges, subgraphs)
     elif mode == "full":
         obj_fun = obj_qaoa_maxcut_full
         grad_fun = grad_qaoa_maxcut_full
+        opt_args = (depth, graph)
     else:
         raise ValueError
 
@@ -181,25 +185,70 @@ def optimize_circ_params(mode,
 
     return np.array(obj_over_opti), opt_result
 
+# def qutip_qaoa_maxcut(params, depth, graph):
+#     z_ops = {}
+#     num_nodes = len(graph.nodes)
+#     Z = qutip.sigmaz()
+#     I = qutip.qeye(2)
+#     sx = qutip.sigmax()
+#     I_tot = qutip.tensor([I] * num_nodes)
+#     for i, node in enumerate(graph.nodes):
+#         z_ops[node] = qutip.tensor([I] * i + [Z] + [I] * (num_nodes - i - 1))
+#
+#     H = 0
+#     for edge in graph.edges:
+#         H += -1/2 * (I_tot - z_ops[edge[0]] * z_ops[edge[1]])
+#
+#     diag_entries_H = H.diag()
+#     psi0 = qutip.basis([2] * num_nodes)
+#     psi0 = qutip.hadamard_transform(N = num_nodes) * psi0
+#
+#     gamma = params[:depth]
+#     beta = params[depth:]
+#
+#     psi_after_step = psi0
+#
+#     for step_num in range(depth):
+#         Up = np.diag(np.exp(-1j * gamma[step_num] * diag_entries_H))
+#         Up = qutip.Qobj(Up, dims = H.dims, shape = H.shape)
+#
+#         Um = I_tot
+#         Ux = (-1j * beta[step_num]/2 * sx).expm()
+#         for i, node in enumerate(graph.nodes):
+#             Um *= qutip.tensor([I] * i + [Ux] + [I] * (num_nodes - i - 1))
+#
+#         psi_after_step = Up * psi_after_step
+#         psi_after_step = Um * psi_after_step
+#
+#     return qutip.expect(H, psi_after_step)
+
 if __name__ == "__main__":
-    lattice = graphs.define_lattice((1,10))
-    graph = graphs.create_random_connectivity(lattice)
-    depth = 2
+    """
+    Usage example.
+    """
+    lattice = graphs.define_lattice((1, 8))
+    # defines a 1D lattice of 20 sites
+    graph = graphs.create_random_connectivity(lattice, p = 0.7)
+    # defines a planar graph on the lattice by keeping each edge in the lattice
+    # with a probability p
+    depth = 3
+    # depth of QAOA circuit
 
     key = jax.random.PRNGKey(60)
+    # needed for jax RNG, substitute 60 for any other int to change key
     params_init = jax.random.uniform(key = key, shape = (2 * depth,),
                                      minval = 0, maxval = 2 * np.pi)
+    # QAOA obj is periodic in the parameters.
 
-    # obj_full = obj_qaoa_maxcut_full(params_init, depth, graph)
-    # obj_cone = obj_qaoa_maxcut_cone(params_init, depth, graph)
-
+    edges, subgraphs = graphs.all_dist_p_subgraphs(graph, p = depth)
     obj_over_opti, opt_result = optimize_circ_params("cone",
-                                params_init, depth, graph,
+                                params_init, depth, graph, edges, subgraphs,
                                 num_iters = 50)
+    # num_iters = max number of iterations you want to run
+    # "cone" is to call the optimizer with the light cone-restricted
+    # version of the QAOA evolution. Fast for small circuit depths.
+    # Roughly, max size of vector ~ p *
 
+    print(obj_qaoa_maxcut_cone(jnp.array(opt_result.x), depth, graph, edges, subgraphs))
+    # print(qutip_qaoa_maxcut(opt_result.x, depth, graph))
     print(obj_qaoa_maxcut_full(jnp.array(opt_result.x), depth, graph))
-    print(obj_qaoa_maxcut_cone(jnp.array(opt_result.x), depth, graph))
-
-
-    # print(obj_full)
-    # print(obj_cone)
