5b7db5ecc26b016a6067e850ba58563dde6e010d
Sattwik Deb Mishra <sdmishra@stanford.edu>
2023-05-08 12:32:25 -0700

    Finer N = 32 sweep

--- analyse_heisenberg_results.py
+++ analyse_heisenberg_results.py
@@ -24,20 +24,20 @@ CB_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
                   '#f781bf', '#a65628', '#984ea3',
                   '#999999', '#e41a1c', '#dede00']
 
-data_path = "../vqa_data/0508/20230508-025157/"
+# data_path = "../vqa_data/0508/20230508-092949/"
 
-N_list = [16,]
-p_list = [0.03, 0.1, 0.3]
-d_list = [8, 10, 12, 20]
-D_list = [2, 8, 32, 64, 128]
-theta_list = [0.03, 0.1, 1.0]
-
-# data_path = "../vqa_data/0508/20230508-032044/"
-# N_list = [32]
+# N_list = [16,]
 # p_list = [0.03, 0.1, 0.3]
-# d_list = [4, 6, 8, 10, 12, 20, 24]
-# D_list = [2, 8, 16, 32, 64, 128, 256]
-# theta_list = [0.01, 0.1, 1.0]
+# d_list = [8, 10, 12, 20]
+# D_list = [2, 8, 32, 64, 128]
+# theta_list = [0.03, 0.1, 1.0]
+
+data_path = "../vqa_data/0508/20230508-101016/"
+N_list = [32]
+p_list = [0.03, 0.1, 0.3]
+d_list = [4, 6, 8, 10, 12, 20, 24]
+D_list = [2, 8, 16, 32, 64,]
+theta_list = [0.01, 0.1, 1.0]
 
 num_D = len(D_list)
 num_N = len(N_list)
@@ -59,25 +59,21 @@ heis_val_list = np.zeros((num_N, num_seeds, num_p, num_D, num_theta, num_d), dty
 for i_N, N in enumerate(N_list):
     for i_seed, seed in enumerate(N + np.array(range(num_seeds))):
         for i_p, p in enumerate(p_list):
-            i_theta = i_p
-            theta = p
             # for i_k, k_dual in enumerate([1, N]):
             for i_D, D in enumerate(D_list):
                 for i_d, d in enumerate(d_list):
-                    #  for i_theta, theta in enumerate(theta_list):
-                    # for i_theta, theta in enumerate(p_list):
+                     for i_theta, theta in enumerate(theta_list):
+                        fname = "heis1D-N-" + str(N) + "-d-" + str(d) + "-seed-" + \
+                                str(seed) + "-theta-" + f'{theta:.4f}' + \
+                                "-p-" + str(p) + "-D-" + str(D) + ".pkl"
+                        with open(os.path.join(data_path, fname), 'rb') as result_file:
+                            print("Reading file: " + os.path.join(data_path, fname))
 
-                    fname = "heis1D-N-" + str(N) + "-d-" + str(d) + "-seed-" + \
-                            str(seed) + "-theta-" + f'{theta:.4f}' + \
-                            "-p-" + str(p) + "-D-" + str(D) + ".pkl"
-                    with open(os.path.join(data_path, fname), 'rb') as result_file:
-                        print("Reading file: " + os.path.join(data_path, fname))
+                            heis_val, heis_bound, dual_bound = pickle.load(result_file)
 
-                        heis_val, heis_bound, dual_bound = pickle.load(result_file)
-
-                        dual_bound_list[i_N, i_seed, i_p, i_D, i_theta, i_d] = dual_bound
-                        heis_bound_list[i_N, i_seed, i_p, i_D, i_theta, i_d] = heis_bound
-                        heis_val_list[i_N, i_seed, i_p, i_D, i_theta, i_d] = heis_val
+                            dual_bound_list[i_N, i_seed, i_p, i_D, i_theta, i_d] = dual_bound
+                            heis_bound_list[i_N, i_seed, i_p, i_D, i_theta, i_d] = heis_bound
+                            heis_val_list[i_N, i_seed, i_p, i_D, i_theta, i_d] = heis_val
 
 heis_bound_list = np.array(heis_bound_list)
 dual_bound_list = np.array(dual_bound_list)
@@ -90,34 +86,33 @@ norm = 2 * N
 for i_N, N in enumerate(N_list):
     for i_seed, seed in enumerate(N + np.array(range(num_seeds))):
         for i_p, p in enumerate(p_list):
-            i_theta = i_p
-            theta = p
-            # for i_theta, theta in enumerate(theta_list):
+            for i_theta, theta in enumerate(theta_list):
+                fig = plt.figure(figsize=(3.5284350352843505, 2.469904524699045))
+                ax = fig.add_subplot(111)
 
-            fig = plt.figure(figsize=(3.5284350352843505, 2.469904524699045))
-            ax = fig.add_subplot(111)
+                for i_D, D in enumerate(D_list):
+                    if i_D >= 2:
+                        
+                        ax.plot([1 + 2 * d for d in d_list], (dual_bound_list[i_N, i_seed, i_p, i_D, i_theta, :] - clean_sol)/norm, 
+                                label = "Dual, D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
+                                marker = '.', markersize = 4)
+                        ax.plot([1 + 2 * d for d in d_list], (heis_bound_list[i_N, i_seed, i_p, i_D, i_theta, :] - clean_sol)/norm, ls = "--", 
+                                label = "Heis.,  D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
+                                marker = 'D', markersize = 3)
+                        
+                        # ax.plot([1 + 2 * d for d in d_list], dual_bound_list[i_N, i_seed, i_p, i_D, i_theta, :], 
+                        #         label = "Dual, D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
+                        #         marker = '.', markersize = 4)
+                        # ax.plot([1 + 2 * d for d in d_list], heis_bound_list[i_N, i_seed, i_p, i_D, i_theta, :], ls = "--", 
+                        #         label = "Heis.,  D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
+                        #         marker = 'D', markersize = 3)
 
-            for i_D, D in enumerate(D_list):
-                if i_D >= 2:
-                    # ax.plot([1 + 2 * d for d in d_list], (dual_bound_list[i_N, i_seed, i_p, i_D, i_theta, :] - clean_sol)/norm, 
-                    #         label = "Dual, D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
-                    #         marker = '.', markersize = 4)
-                    # ax.plot([1 + 2 * d for d in d_list], (heis_bound_list[i_N, i_seed, i_p, i_D, i_theta, :] - clean_sol)/norm, ls = "--", 
-                    #         label = "Heis.,  D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
-                    #         marker = 'D', markersize = 3)
-                    
-                    ax.plot([1 + 2 * d for d in d_list], dual_bound_list[i_N, i_seed, i_p, i_D, i_theta, :], 
-                            label = "Dual, D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
-                            marker = '.', markersize = 4)
-                    ax.plot([1 + 2 * d for d in d_list], heis_bound_list[i_N, i_seed, i_p, i_D, i_theta, :], ls = "--", 
-                            label = "Heis.,  D = " + str(D), color = 'C' + str(i_D), lw = 0.75, 
-                            marker = 'D', markersize = 3)
+                        # ax.plot([1 + 2 * d for d in d_list], (outputs - clean_sol)/norm, label = "Output", 
+                        #         color = 'k', ls = ":")
 
-            # ax.plot([1 + 2 * d for d in d_list], (outputs - clean_sol)/norm, label = "Output", 
-            #         color = 'k', ls = ":")
-            # ax.set_yscale('log')
-            ax.legend()
-            ax.set_title("N = " + str(N) + r", $\theta$ = " + f'{theta:.2f}' + r"$p$ = " + str(p))
-            plt.tight_layout()
-            figname = "heis_test_N_" + str(N) + "_p_" + str(p) + "_theta_" + f'{theta:.2f}' + ".pdf"
-            plt.savefig(os.path.join(data_path, figname), bbox_inches = 'tight', format = 'pdf')
+                ax.set_yscale('log')
+                ax.legend()
+                ax.set_title("N = " + str(N) + r", $\theta$ = " + f'{theta:.2f}' + r"$p$ = " + str(p))
+                plt.tight_layout()
+                figname = "heis_test_N_" + str(N) + "_p_" + str(p) + "_theta_" + f'{theta:.2f}' + ".pdf"
+                plt.savefig(os.path.join(data_path, figname), bbox_inches = 'tight', format = 'pdf')
